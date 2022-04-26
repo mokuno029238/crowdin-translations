a4580f46df67b200fe45a03026bf8646d22faea3
kevin_kj_peng <kevin_kj_peng@gss.com.tw>
2022-04-26 16:59:30 +0800

    feat: add color page
    - disable .net 6 null state analysis

---
+++ src/Crowdin.Translations.Web/Datas/ColorData.cs
@@ -0,0 +1,9 @@
+﻿namespace Crowdin.Translations.Web.Datas
+{
+    public class ColorData
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
--- src/Crowdin.Translations.Web/Pages/Index.cshtml.cs
+++ src/Crowdin.Translations.Web/Pages/Index.cshtml.cs
@@ -1,19 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Crowdin.Translations.Web.Datas;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Crowdin.Translations.Web.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly string _defaultColorId;
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+
+            Colors = new List<ColorData>
+            {
+                new ColorData { Id = "red", Name = "Red" },
+                new ColorData { Id = "yellow", Name = "Yellow" },
+                new ColorData { Id = "blue", Name = "Blue" }
+            };
+            _defaultColorId = Colors.First().Id;
         }
+
+        public IList<ColorData> Colors { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string ColorId { get; set; }
+
+        public ColorData SelectedColor { get; set; }
 
         public void OnGet()
         {
-
+            var colorId = string.IsNullOrEmpty(ColorId) ? _defaultColorId : ColorId;
+            var color = Colors.FirstOrDefault(c => c.Id == colorId);
+            color ??= Colors.First();
+            ColorId = color.Id;
+            SelectedColor = color;
         }
     }
 }
